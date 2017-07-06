218a1b4d84c5ac6d5b9ac937c1f41b10de5f4c52
denchikUniversal <denchikrandom@mail.ru>
2017-07-06 19:24:25 +0300

    first upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+   <button @click="currentComponent = 'app-blue'">Blue</button>
+   <button @click="currentComponent = 'app-green'">Green</button>
+   <button @click="currentComponent = 'app-red'">Red</button>
+   <hr>
+   <keep-alive>
+     <component :is="currentComponent"></component>
+   </keep-alive>
+    
+  </div>
+</template>
+
+<script>
+
+import Blue from './components/Blue.vue';
+import Green from './components/Green.vue';
+import Red from './components/Red.vue';
+
+export default {
+  data () {
+    return {
+      currentComponent: 'app-blue'
+    }
+  },
+  components: {
+    'app-blue': Blue,
+    'app-green': Green,
+    'app-red': Red
+  }
+}
+</script>
+
+<style>
+div{
+  width: 400px;
+  margin: auto;
+}
+</style>
---
+++ src/components/Red.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>This is {{ redTitle }} title!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      redTitle: 'red'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div{
+	border: 1px solid #666;
+	font-size: 24px;
+	color: #fff;
+	font-weight: bold;
+	text-align: center;
+	box-shadow: 1px 1px 2px #333;
+	padding: 30px;
+	margin: auto;
+	background-color: red;
+}
+</style>
